ab6b04d950868f5085e1e218b27683d22a209241
arisa <arisa.okasaka@gmail.com>
2021-03-09 16:29:17 +0900

    エラー解消

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 class Language extends React.Component{
-
-    
-
     render(){
         return(
-            <div className='language-card'>
-                <div className='language-item'>
-                    <img src={this.props.image}/>
-                    <p>{this.props.name}</p>
-                </div>
+            <div className='language'>
+                {this.props.list.map((item)=>{
+                    return(
+                        <div className='language-card' key={'language_' + item.id}>
+                            <div className='language-item'>
+                            <img src={item.image} alt="言語ごとの画像"/>
+                            <p>{item.name}</p>
+                        </div>
+                     </div>
+                    );
+                })}
             </div>
         );
     }
